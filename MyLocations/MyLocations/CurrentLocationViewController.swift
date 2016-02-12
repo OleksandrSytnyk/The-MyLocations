f0052a5cfdc6f4369642d80c6087d42d570e86c9
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-12 19:20:21 +0200

    Adding labels and buttons on the Tag screen

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -5,7 +5,14 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -14,6 +21,10 @@ class CurrentLocationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getLocation() {
+        // do nothing yet
     }
 
 
