bde340e80bd29aa9d59e541938bc95fdf7354095
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-12 15:58:02 +0200

    Tabbed Application template very easy customized

---
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,21 @@
+//  Created by MyMacbook on 2/12/16.
+//  Copyright © 2016 Oleksandr. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
