a108c72798a6d8f59465e49f8cc8367ef4b63f6c
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-21 17:16:48 +0200

    Adding LocationsDetailsViewController and controls

---
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by MyMacbook on 2/21/16.
+//  Copyright © 2016 Oleksandr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBAction func done() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancel() {
+            dismissViewControllerAnimated(true, completion: nil)
+    }
+}
