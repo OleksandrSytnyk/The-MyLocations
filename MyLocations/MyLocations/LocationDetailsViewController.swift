8f4f1d725e9460d430eac988aa3a4f3eab997026
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-22 09:08:45 +0200

    prepareForSegue method and filling labels
    global const for dateFormatter

--- MyLocations/MyLocations/LocationDetailsViewController.swift
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 import UIKit
+import CoreLocation
+
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    return formatter }()
 
 class LocationDetailsViewController: UITableViewController {
     
@@ -17,6 +24,54 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+ 
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        if let placemark = placemark {
+        addressLabel.text = stringFromPlacemark(placemark)
+    } else {
+        addressLabel.text = "No Address Found"
+        }
+        dateLabel.text = formatDate(NSDate())
+    }
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String {
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        if let s = placemark.locality {
+                text += s + ", "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+                text += s
+        }
+        return text
+    }
+    
+    func formatDate(date: NSDate) -> String {
+                return dateFormatter.stringFromDate(date)
+    }
+
     
     @IBAction func done() {
         dismissViewControllerAnimated(true, completion: nil)
