91a1dedf9eb80675517487404f35f3a78386f49d
OleksandrSytnyk <amsitnik@gmail.com>
2016-02-25 13:00:56 +0200

    animation with a lot more options

--- MyLocations/MyLocations/HudView.swift
+++ MyLocations/MyLocations/HudView.swift
@@ -43,9 +43,10 @@ class HudView: UIView {
         alpha = 0
         transform = CGAffineTransformMakeScale(1.3, 1.3)
         
-        UIView.animateWithDuration(0.3, animations: {
+        UIView.animateWithDuration(0.3, delay: 1, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: [], animations: {
         self.alpha = 1
-        self.transform = CGAffineTransformIdentity})
+            self.transform = CGAffineTransformIdentity},
+            completion: nil)
         }
     }
 }
